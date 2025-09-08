55b32a20454f84b67b7361b3804180d639da4bd8
neslol <obmenfx@gmail.com>
2025-09-08 11:33:54 +0200

    ok

--- src/Initialize.js
+++ src/Initialize.js
@@ -5,9 +5,11 @@ export default class Initialize {
     this.counter = 0;
     const myName = "Arseniy";
     let age = 17;
+
+    this.myFunc(myName, age);
   } // END constructor
 
-  myFunc() {
-    console.log("jeg er i en function");
+  myFunc(n, a) {
+    console.log(n, a);
   }
 } // END class
